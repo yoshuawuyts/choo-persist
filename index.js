492e89a06bbc7783a0400b14de4105f56a5afabc
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2017-03-21 19:04:58 +0100

    Update for choo V5 (#31)
    * update for choo v5
    
    * update for choo v5
    
    * fixup! fix stuff
    
    * fixup! fix tests

--- index.js
+++ index.js
@@ -1,94 +1,33 @@
-const explain = require('explain-error')
-const window = require('global/window')
-const Idb = require('idb-wrapper')
-const assert = require('assert')
-const xtend = require('xtend')
+var mutate = require('xtend/mutable')
 
 module.exports = persist
 
-// create a choo persistance plugin that stores stuff in indexedDB
-// (obj?, fn(obj) -> null
-function persist (opts, cb) {
-  if (!cb) {
-    cb = opts
-    opts = {}
-  }
+function persist (opts) {
+  opts = opts || {}
 
-  assert.equal(typeof opts, 'object', 'choo-persist: opts should be an object')
-  assert.equal(typeof cb, 'function', 'choo-persist: cb should be an function')
+  var name = opts.name || 'choo-persist'
+  var filter = opts.filter
 
-  const name = opts.name || 'app'
-  const filter = opts.filter || noop
-  if (!window.indexedDB) return cb({})
+  return function (state, bus) {
+    var savedState = null
+    try {
+      savedState = JSON.parse(window.localStorage.getItem(name))
+    } catch (e) {
+      savedState = {}
+    }
 
-  const db = new Idb({
-    dbVersion: 1,
-    storeName: name,
-    keyPath: 'id',
-    autoIncrement: false,
-    onStoreReady: onStoreReady
-  })
+    mutate(state, savedState)
+    bus.on('*', listener)
 
-  function onStoreReady () {
-    getState(db, function (err, state) {
-      if (err) throw err
-      cb({
-        onStateChange: createStateChange(db, filter),
-        wrapInitialState: function (appState) {
-          return xtend(appState, state)
-        }
-      })
+    bus.on('clear', function () {
+      bus.removeListener('*', listener)
+      window.localStorage.removeItem(name)
+      bus.emit('log:warn', 'Wiping localStorage ' + name)
     })
+
+    function listener (eventName, data) {
+      var savedState = filter ? filter(state) : state
+      window.localStorage.setItem(name, JSON.stringify(savedState))
+    }
   }
-}
-
-// persist stuff to the local database
-// (obj) -> (obj, obj, obj, str, fn) -> null
-function createStateChange (db, filter) {
-  return function onStateChange (state, data, prev, caller, createSend) {
-    if (filter) state = filter(state)
-    setState(db, state, function (err) {
-      if (err) {
-        const send = createSend('choo-persist')
-        send('error', err)
-      }
-    })
-  }
-}
-
-// get the initial values from the database
-// (obj, fn(err?, obj)) -> null
-function getState (db, cb) {
-  db.getAll(onSuccess, onError)
-
-  function onSuccess (data) {
-    var state = data[data.length - 1] || {}
-    delete state.id
-    delete state.location
-    cb(null, state)
-  }
-
-  function onError (err) {
-    const nwErr = explain(err, 'choo-persist: something went wrong accessing the database while starting up')
-    cb(nwErr)
-  }
-}
-
-// set values on the database
-// (obj, obj, fn(err?, obj)) -> null
-function setState (db, state, cb) {
-  db.put(xtend(state), onSuccess, onError)
-
-  function onSuccess () {
-    cb()
-  }
-
-  function onError (err) {
-    const newErr = explain(err, 'choo-persist: expected database write to be successful')
-    cb(newErr)
-  }
-}
-
-function noop (state) {
-  return state
 }
